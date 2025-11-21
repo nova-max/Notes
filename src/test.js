d9a1d9ba1e6fe8ef316ab418f6d4a8b68ec29362
Marco Giron diaz <marco1236677@gmail.com>
2025-11-21 17:24:51 -0500

    Deploy Vite build to GitHub Pages

--- src/test.js
+++
@@ -1,3 +0,0 @@
-console.log("Test.js cargado correctamente");
-document.getElementById('root').innerHTML = '<h1 style="color:green">¡SISTEMA FUNCIONANDO!</h1><p>El archivo JS se cargó.</p>';
-alert("¡El sistema está vivo!");
